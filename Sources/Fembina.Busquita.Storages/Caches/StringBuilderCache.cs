912b9b7859de714a1fa7bbff16afdc0f1a748b56
Rima Falko <fembina@pm.me>
2025-04-14 18:21:37 +0200

    Remove unnecessary documentation comments in StringBuilderCache for improved clarity

--- Sources/Fembina.Busquita.Storages/Caches/StringBuilderCache.cs
+++ Sources/Fembina.Busquita.Storages/Caches/StringBuilderCache.cs
@@ -2,26 +2,14 @@ using System.Text;
 
 namespace Fembina.Busquita.Storages.Caches;
 
-/// <summary>
-/// A cache for <see cref="StringBuilder"/> instances.
-/// This class is designed to improve performance by reusing StringBuilder instances.
-/// This class is thread-safe.
-/// Caches a single StringBuilder instance or creates a new one if the cached instance is not available.
-/// </summary>
 public sealed class StringBuilderCache
 {
-    // I'm caching the TrySet delegate to avoid creating a new delegate instance every time
     private readonly Action<StringBuilder> _trySetDelegate;
 
     private readonly int _capacity;
 
-    /// I'm caching the last set StringBuilder instance to avoid creating a new one every time
     private StringBuilder? _builder;
 
-    /// <summary>
-    /// Creates a new instance of the <see cref="StringBuilderCache"/> class with the specified capacity.
-    /// </summary>
-    /// <param name="capacity">The initial capacity of the StringBuilder.</param>
     public StringBuilderCache(int capacity = 256)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(capacity, nameof(capacity));
@@ -30,14 +18,6 @@ public sealed class StringBuilderCache
         _trySetDelegate = TrySet;
     }
 
-    /// <summary>
-    /// Gets or creates a <see cref="ShotTimeValue{StringBuilder}"/> instance.
-    /// </summary>
-    /// <returns>
-    /// Returns a <see cref="ShotTimeValue{StringBuilder}"/> instance
-    /// containing a StringBuilder with the specified capacity.
-    /// </returns>
-    /// <remarks>You should dispose of returned ShotTimeValue when you're done with it.</remarks>
     public ShotTimeValue<StringBuilder> GetOrCreate()
     {
         var builder = Interlocked.Exchange(ref _builder, null);

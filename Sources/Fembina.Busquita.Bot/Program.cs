81a3ac5a934719a15604d78b009bd8e67edd829b
Rima Falko <rima@falko.email>
2025-09-17 00:50:57 +0200

    fix namespaces on startup

--- Sources/Fembina.Busquita.Bot/Program.cs
+++ Sources/Fembina.Busquita.Bot/Program.cs
@@ -3,10 +3,12 @@ using System.Logging.Renderers;
 using System.Logging.Runtimes;
 using System.Logging.Targets;
 using System.Logging.Builders;
+using Falko.Talkie.Hosting;
 using Fembina.Busquita.Bot.Behaviors;
 using Fembina.Busquita.Bot.Extensions;
 using Fembina.Busquita.Bot.Integrations;
 using Fembina.Busquita.Storages.Assets;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using var loggerRuntime = LoggerRuntime.Global;
